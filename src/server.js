9d7ca49c28b3d6daf4cfb0c8887f581ee9550a4e
Amalu Sajeev <amalusajeev@pop-os.localdomain>
2023-08-04 04:30:30 +0530

    added staticfile serving for public directory

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
 import http from "http";
+import path from "path";
+import fs from "fs/promises";
+import { existsSync } from "fs";
+
 import { DEFAULT_LISTENING_PORT } from "./utils/constants.js";
 import { HTTP_ERRORS } from "./utils/errors.js";
 
@@ -27,11 +31,29 @@ export class Server {
       if (routeHandler) {
         routeHandler(request, response);
       } else {
-        this.handleError(response, ...NOT_FOUND);
+        this.staticFileServe(request.url, response);
+        // this.handleError(response, ...NOT_FOUND);
       }
     } catch (error) {
       console.error(error);
       this.handleError(response, ...INTERNAL_SERVER_ERROR);
+    }
+  }
+  async staticFileServe(filePath, response) {
+    const { NOT_FOUND } = HTTP_ERRORS;
+    const fullFilePath = path.join(process.cwd(), "public", filePath);
+    const stats = await fs.lstat(fullFilePath);
+    const isFile = stats.isFile();
+    const parsedFilePath = !isFile
+      ? path.join(fullFilePath, "index.html")
+      : fullFilePath;
+    const pathExists = existsSync(parsedFilePath);
+    console.log(parsedFilePath, filePath, stats.isFile());
+    if (pathExists) {
+      const data = await fs.readFile(parsedFilePath, { encoding: "utf-8" });
+      response.end(data);
+    } else {
+      this.handleError(response, ...NOT_FOUND);
     }
   }
   handleError(response, statusCode, message) {
